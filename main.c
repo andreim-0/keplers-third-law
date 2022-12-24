460bd219353f9d30f620bf339706cbc4ed78c92f
Andrei M <andrei.mitroi444@gmail.com>
2022-12-24 14:34:21 +0100

    Adding Linux Support

--- main.c
+++ main.c
@@ -7,7 +7,9 @@ int main(int argc, char* argv[]){
     printf("Enter the distance of this object from the sun (in the AU unit) : ");
     scanf("%d", &input);
     int result = sqrt(input * input * input);
-	printf("This object would take %d years to orbit the sun.\n", result);
-    system("pause");
-	return 0;
+    printf("This object would take %d years to orbit the sun.\n", result);
+    #ifdef _WIN32
+        system("pause");
+    #endif	
+    return 0;
 }
